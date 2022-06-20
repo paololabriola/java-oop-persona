4cbbd9cb69fd61d5d340b2ad7582ea0783c6ab3a
paololabriola <paololabriola02@gmail.com>
2022-06-20 12:58:43 +0200

    Created classes

--- src/Indirizzo.java
+++ src/Indirizzo.java
@@ -1,4 +1,40 @@
 
 public class Indirizzo {
 
+	private String viaIndirizzo, cittaIndirizzo;
+	private int numeroCivicoIndirizzo;
+	
+	public Indirizzo(String viaIndirizzo, String cittaIndirizzo, int numeroCivicoIndirizzo) {
+		
+		super();
+		this.viaIndirizzo = viaIndirizzo;
+		this.cittaIndirizzo = cittaIndirizzo;
+		this.numeroCivicoIndirizzo = numeroCivicoIndirizzo;
+		
+	}
+
+	public String getViaIndirizzo() {
+		return viaIndirizzo;
+	}
+
+	public void setViaIndirizzo(String viaIndirizzo) {
+		this.viaIndirizzo = viaIndirizzo;
+	}
+
+	public String getCittaIndirizzo() {
+		return cittaIndirizzo;
+	}
+
+	public void setCittaIndirizzo(String cittaIndirizzo) {
+		this.cittaIndirizzo = cittaIndirizzo;
+	}
+
+	public int getNumeroCivicoIndirizzo() {
+		return numeroCivicoIndirizzo;
+	}
+
+	public void setNumeroCivicoIndirizzo(int numeroCivicoIndirizzo) {
+		this.numeroCivicoIndirizzo = numeroCivicoIndirizzo;
+	}
+	
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,16 @@
 
 public class Persona {
 
+	private String nomePersona, cognomePersona;
+	private Indirizzo indirizzoPersona;
+	
+	public Persona(String nomePersona, String cognomePersona, Indirizzo indirizzoPersona) {
+		
+		super();
+		this.nomePersona = nomePersona;
+		this.cognomePersona = cognomePersona;
+		this.indirizzoPersona = new Indirizzo(indirizzoPersona.getViaIndirizzo(), indirizzoPersona.getCittaIndirizzo(), indirizzoPersona.getNumeroCivicoIndirizzo()); 
+		
+	}
+	
 }
